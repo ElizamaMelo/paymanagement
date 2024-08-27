8923e74272779c344144db3f1907e2060b788447
Elizama Melo <eliizamello@gmail.com>
2024-08-27 15:32:00 +0200

    initial commit

---
+++ src/test/java/com/exis/paymanagement/PaymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exis.paymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
